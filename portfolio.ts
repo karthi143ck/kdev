5f2da5142b20631eff4e1ef89b28eb19b446d5d5
karthi143ck-admin <karthi143ck@gmail.com>
2023-07-15 18:17:58 +0530

    initial commit

---
+++ portfolio.ts
@@ -0,0 +1,276 @@
+import emoji from 'react-easy-emoji';
+import {
+  EducationType,
+  ExperienceType,
+  FeedbackType,
+  ProjectType,
+  SkillsSectionType,
+  SkillBarsType,
+  SEODataType,
+  SocialLinksType,
+  GreetingsType,
+} from './types/sections';
+
+export const greetings: GreetingsType = {
+  name: 'Karthick K',
+  title: "Hi all, I'm Karthick",
+  description:
+    "I'm passionate Full Stack web developer having an experience of web applications with Python, Flask, React.js, Next.js and Blockchain development on Ethereum, Solidity, Web3.js, Moralis, and Brownie Framework.",
+  resumeLink:
+    'https://drive.google.com/file/d/1l2MG8U2jBWAYODEor8vJ5KJmeXhaPWMi/preview',
+};
+
+export const openSource = {
+  githubUserName: 'karthi143ck',
+};
+
+export const contact = {};
+
+export const socialLinks: SocialLinksType = {
+  url: 'https://karthi143ck.github.io/',
+  linkedin: 'https://www.linkedin.com/in/karthick-k-3959251aa/',
+  github: 'https://github.com/karthi143ck',
+  instagram: 'https://www.instagram.com/karthi143ck',
+  facebook: 'https://www.facebook.com/100040440710650',
+  twitter: 'https://twitter.com/karthi143ck',
+};
+
+export const skillsSection: SkillsSectionType = {
+  title: 'What I do',
+  subTitle: 'CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK',
+  data: [
+    {
+      title: 'Full Stack Development',
+      lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji(
+          '⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js'
+        ),
+        emoji('⚡ Building responsive static websites using Next.js'),
+        emoji('⚡ Building RESTful APIs in Flask & Flask REST Framework'),
+      ],
+      softwareSkills: [
+        {
+          skillName: 'HTML-5',
+          fontAwesomeClassName: 'vscode-icons:file-type-html',
+        },
+        {
+          skillName: 'CSS-3',
+          fontAwesomeClassName: 'vscode-icons:file-type-css',
+        },
+        {
+          skillName: 'JavaScript',
+          fontAwesomeClassName: 'logos:javascript',
+        },
+        {
+          skillName: 'Reactjs',
+          fontAwesomeClassName: 'vscode-icons:file-type-reactjs',
+        },
+        {
+          skillName: 'Nextjs',
+          fontAwesomeClassName: 'vscode-icons:file-type-light-next',
+        },
+        {
+          skillName: 'Python',
+          fontAwesomeClassName: 'logos:python',
+        },
+        {
+          skillName: 'Flask',
+          fontAwesomeClassName: 'vscode-icons:file-type-django',
+        },
+
+        {
+          skillName: 'Redux',
+          fontAwesomeClassName: 'logos:redux',
+        },
+        {
+          skillName: 'NPM',
+          fontAwesomeClassName: 'logos:npm-icon',
+        },
+        {
+          skillName: 'Yarn',
+          fontAwesomeClassName: 'logos:yarn',
+        },
+      ],
+    },
+    {
+      title: 'Cloud Infra-Architecture',
+      lottieAnimationFile: '/lottie/skills/cloudinfra.json', // Path of Lottie Animation JSON File
+      skills: [
+        emoji('⚡ Experience of working on multiple cloud platforms'),
+        emoji(
+          '⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases'
+        ),
+        emoji(
+          '⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions'
+        ),
+      ],
+      softwareSkills: [
+        // ? Check README To get icon details
+        {
+          skillName: 'AWS',
+          fontAwesomeClassName: 'logos:aws',
+        },
+        {
+          skillName: 'Azure',
+          fontAwesomeClassName: 'logos:microsoft-azure',
+        },
+        {
+          skillName: 'Heroku',
+          fontAwesomeClassName: 'logos:heroku-icon',
+        },
+        {
+          skillName: 'PostgreSQL',
+          fontAwesomeClassName: 'logos:postgresql',
+        },
+        {
+          skillName: 'Github',
+          fontAwesomeClassName: 'akar-icons:github-fill',
+        },
+        {
+          skillName: 'Docker',
+          fontAwesomeClassName: 'logos:docker-icon',
+        },
+        {
+          skillName: 'Github Actions',
+          fontAwesomeClassName: 'logos:github-actions',
+        },
+        {
+          skillName: 'Cloudinary',
+          fontAwesomeClassName: 'logos:cloudinary',
+        },
+        {
+          skillName: 'Nginx',
+          fontAwesomeClassName: 'logos:nginx',
+        },
+        {
+          skillName: 'Sentry',
+          fontAwesomeClassName: 'logos:sentry-icon',
+        },
+      ],
+    },
+  ],
+};
+
+export const SkillBars: SkillBarsType[] = [
+  {
+    Stack: 'Frontend/Design', //Insert stack or technology you have experience in
+    progressPercentage: '90', //Insert relative proficiency in percentage
+  },
+  {
+    Stack: 'Backend',
+    progressPercentage: '70',
+  },
+  {
+    Stack: 'Programming',
+    progressPercentage: '60',
+  },
+];
+
+export const educationInfo: EducationType[] = [
+  {
+    schoolName: 'Harvard University',
+    subHeader: 'Master of Science in Computer Science',
+    duration: 'September 2017 - April 2019',
+    desc: 'Participated in the research of XXX and published 3 papers.',
+    grade: 'Grade A',
+    descBullets: [
+      'Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    ],
+  },
+];
+
+export const experience: ExperienceType[] = [
+  {
+    role: 'Frontend Developer',
+    company: 'Duseca Software',
+    companyLogo: '/img/icons/common/dusecaSoftware.jpg',
+    date: 'Apr 2022 – Jun 2022',
+    desc: 'I worked as frontend web developer to design web based admin dashboards for mulitple apps using Flask, Bootstrap, jQuery, Chart.js and some other libraries.',
+  },
+  {
+    role: 'API Engineer',
+    company: 'Duseca Software',
+    companyLogo: '/img/icons/common/dusecaSoftware.jpg',
+    date: 'Jan 2022 – Mar 2022',
+    desc: 'I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Flask, FlaskRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Flask Server and deployed the API on Heroku Server.',
+    // descBullets: [
+    // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+    // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    // ],
+  },
+  {
+    role: 'Full Stack Developer',
+    company: 'Bleed-AI',
+    companyLogo: '/img/icons/common/bleedAI.jpg',
+    date: 'Sept 2021 - Oct 2021',
+    desc: 'Worked as Flask web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Flask Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.',
+  },
+  {
+    role: 'Backend Developer',
+    company: 'Wapidu',
+    companyLogo: '/img/icons/common/wapidu.jpg',
+    date: 'Sept 2021',
+    desc: 'Worked as a Flask Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Flask REST Framework to integrate Flask Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.',
+  },
+];
+
+export const projects: ProjectType[] = [
+  {
+    name: 'developer-portfolio',
+    desc: 'Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.',
+    github: 'https://github.com/karthi143ck/developer-portfolio',
+    link: 'https://developer-portfolio-karthi143ck.vercel.app/',
+  },
+  {
+    name: 'AtlasMart',
+    desc: 'With Atlas Mart, it’s easy to find the products with the best price and ship them straight to your door.',
+    github: 'https://github.com/karthi143ck/Flask-React-Marketplace',
+  },
+  {
+    name: 'Technota (Forum)',
+    desc: 'Get hands-on experience in technical skills with Technota',
+    github: 'https://github.com/karthi143ck/Flask-react-forum',
+  },
+  {
+    name: 'Shopaza (Ecommerce)',
+    desc: 'Now your reach to all your favorite entertainment and luxury items is just one touch apart',
+    github: 'https://github.com/karthi143ck/Flask-ecommerce',
+  },
+];
+
+export const feedbacks: FeedbackType[] = [
+  {
+    name: 'John Smith',
+    feedback:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+  },
+  {
+    name: 'John Smith',
+    feedback:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
+  },
+];
+
+// option to hide or show the ContactUs component
+export const showContactUs: boolean = true;
+
+// See object prototype on /types/section.ts page
+export const seoData: SEODataType = {
+  title: 'Hanzla Tauqeer',
+  description:
+    'A passionate Full Stack Web Developer and Blockchain Developer.',
+  author: 'Hanzla Tauqeer',
+  image: 'https://avatars3.githubusercontent.com/u/59178380?v=4',
+  url: 'https://developer-portfolio-karthi143ck.vercel.app',
+  keywords: [
+    'Hanzla',
+    'Hanzla Tauqeer',
+    '@karthi143ck',
+    'karthi143ck',
+    'Portfolio',
+    'Hanzla Portfolio ',
+    'Hanzla Tauqeer Portfolio',
+  ],
+};
